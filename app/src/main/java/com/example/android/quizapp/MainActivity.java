5eeeed965636cf6021d1a2f473691e30b1c6d732
Greg <wilusz.gregory@gmail.com>
2018-01-14 14:32:21 +0100

    Added Toast to validate if the name is provided and back buttons

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String userName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +17,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
+     * This method checks if the name is provided.
+     */
+    public void isNameProvided(View view) {
+        EditText enteredName = (EditText) findViewById(R.id.name_field);
+        userName = enteredName.getText().toString();
+        if (userName.equals("")) {
+            Toast.makeText(this, getString(R.string.toast_no_name), Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            setContentView(R.layout.question_one);
+        }
+    }
+
+    /**
      * This method is called when the start quiz button is clicked.
      */
-    public void goToQuiz(View view) {
-        EditText enteredName = (EditText) findViewById(R.id.name_field);
-        String userName = enteredName.getText().toString();
-
+    public void goToFirstQuestion(View view) {
         setContentView(R.layout.question_one);
     }
 
