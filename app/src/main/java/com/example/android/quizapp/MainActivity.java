941747fefe157f4521c64a18cd7d284ee480482a
Greg <wilusz.gregory@gmail.com>
2018-01-22 17:44:50 +0100

    Changing orientation of the app problem resolved

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.quizapp;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
